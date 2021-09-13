67adfe61c388910843f918d9bd105ea45e565453
ArshadSalim-Fortude <ArshadS@fortude.co>
2021-09-13 11:47:28 +0530

    postgraphile library usage setup complete

--- students-postgraphile/src/main.ts
+++ students-postgraphile/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import http from 'http';
+import postgraphile from 'postgraphile';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(
+    postgraphile(
+      process.env.DATABASE_URL || "postgres://postgres:root123@localhost:5432/studentsdb",
+      "public",
+      {
+        watchPg: true,
+        graphiql: true,
+        enhanceGraphiql: true,
+      }
+    )
+  );
+  
+  app.listen(process.env.PORT || 3500);
 }
 bootstrap();
